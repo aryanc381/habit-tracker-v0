5bd556ca97e5fa4e11b6f1d644d37cfce6491c39
Aryan Chauhan <135949281+aryanc381@users.noreply.github.com>
2026-05-28 23:34:11 +0530

    feat: cool frontend upcoming.

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,2 @@
 # Habit-tracker
+When ai takes over the world with cost, I will be the person not touching grass haha lol
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import PageWow from "@/components/ui";
+
+  function App() {
+
+  return (
+    <div className='flex items-center justify-center mt-[10vw]'>
+     <PageWow />
+    </div>
+  )
+}
+
+export default App

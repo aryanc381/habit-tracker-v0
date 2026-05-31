dc5f6233884bff713d60a365a72d3d19fdc97476
Aryan Chauhan <135949281+aryanc381@users.noreply.github.com>
2026-05-31 16:01:24 +0530

    fix(schema): added fixes to the task, ticket schema.

--- backend/src/db/models/goal.model.ts
+++ backend/src/db/models/goal.model.ts
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
-const goals = new mongoose.Schema({
+const goalSchema = new mongoose.Schema({
+    userId: { type: mongoose.Types.ObjectId, required: true },
     name: { type: String, required: true, default: null },
     description: { type: String, required: true, default: null },
     startDate: { type: Date, required: true, default: null },
     etaDate: { type: Date, required: true, default: null },
-    skillIds: { type: [mongoose.Types.ObjectId], ref: "habit_tracker_skills", required: true, default: [] },
+    skillIds: [{ type: mongoose.Types.ObjectId, ref: "habit_tracker_skills", required: true, default: [] }],
     status: { type: String, enum: ["planned", "in_progress", "off-track", "failed", "completed"], required: true, default: null }
 });
 
-export const Goals = mongoose.model("habit_tracker_goals", goals);
+export const Goals = mongoose.model("habit_tracker_goals", goalSchema);
--- backend/src/db/models/tasks.model.ts
+++ backend/src/db/models/tasks.model.ts
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
     title: { type: String, required: true, default: null },
+    skillId: { type: mongoose.Types.ObjectId, ref: "habit_tracker_skills", required: true },
     description: { type: String, required: true, default: null },
     ticketId: { type: mongoose.Types.ObjectId, ref: "habit_tracker_tickets", required: true, default: null },
-    etaTime: { type: Date, required: true, default: null }
+    etaTime: { type: Date, required: true, default: null },
+    status: { type: Boolean, required: true, default: false }
 });
 
-const Task = mongoose.model("habit_tracker_tasks", taskSchema);
+export const Task = mongoose.model("habit_tracker_tasks", taskSchema);

9fda5bdfd604779983908dcb63efebde9f0ba0c2
Aryan Chauhan <135949281+aryanc381@users.noreply.github.com>
2026-05-31 15:35:55 +0530

    fix: doing dumb shit by not ref-ing it.

--- backend/src/db/models/tasks.model.ts
+++ backend/src/db/models/tasks.model.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const taskSchema = new mongoose.Schema({
     title: { type: String, required: true, default: null },
     description: { type: String, required: true, default: null },
-    ticketId: { type: mongoose.Types.ObjectId, required: true, default: null },
+    ticketId: { type: mongoose.Types.ObjectId, ref: "habit_tracker_tickets", required: true, default: null },
     etaTime: { type: Date, required: true, default: null }
 });
 

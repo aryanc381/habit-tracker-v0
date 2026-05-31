09e9077be749dff52154cbe0a462e370170dafa6
Aryan Chauhan <135949281+aryanc381@users.noreply.github.com>
2026-05-31 15:34:55 +0530

    fix(schema): minor key-name fix.

--- backend/src/db/models/tasks.model.ts
+++ backend/src/db/models/tasks.model.ts
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const taskSchema = new mongoose.Schema({
     title: { type: String, required: true, default: null },
     description: { type: String, required: true, default: null },
-    skillId: { type: mongoose.Types.ObjectId, required: true, default: null },
+    ticketId: { type: mongoose.Types.ObjectId, required: true, default: null },
     etaTime: { type: Date, required: true, default: null }
 });
 
-const Tasks = mongoose.model("habit_tracker_tasks", taskSchema);
+const Task = mongoose.model("habit_tracker_tasks", taskSchema);
